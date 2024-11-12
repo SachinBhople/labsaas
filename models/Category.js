331f98875359153b3c1b680170b73fab85fdb9ce
sachin <sachinb0841@gmail.com>
2024-11-12 15:34:17 +0530

    intial setup

---
+++ models/Category.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const categorySchma = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    isDeleted: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+
+}, { timestamps: true })
+
+module.exports = mongoose.models.category || mongoose.model("category", categorySchma)
+
---
+++ routes/customer.route.js
@@ -0,0 +1,24 @@
+const router = require("express").Router()
+const { customerProtected } = require("../middleware/protected")
+const customerController = require("./../controllers/customer.controller")
+
+router
+    .get("/fetch-customer-profile", customerProtected, customerController.fetchCustomerDetails)
+    .post("/update-profile", customerProtected, customerController.updateCustomerDetails)
+    .post("/place-order", customerProtected, customerController.placeOrder)
+    .get("/fetch-adress", customerController.fetchCustomerAddress)
+    .get("/fetch-orders", customerController.fetchOrders)
+    .put("/reschedule-order/:orderId", customerController.rescheduleOrder)
+    .put("/cancle-order/:orderId", customerController.cancleOrder)
+
+    // medical
+    .post("/placeMedicalOrder", customerController.placeMedicalOrder)
+    .get("/getMedical-Order-Details/:medicalId", customerController.getMedicalOrderDetails)
+    .get("/get-Medical-Order", customerController.getAllMedicalOrders)
+    .put("/cancel-Medical-Order/:medicalId", customerController.cancelMedicalOrder)
+    .post("/add-Medical-Order", customerController.addMedicalOder)
+    .post("/find-doctor", customerController.findDoctor)
+    .get("/fetch-all-doctor", customerController.fetchAllDoctor)
+    .post("/book-doctor-appointment", customerController.bookDoctorAppointment)
+
+module.exports = router
